bf111d3a8f6fd648620926a9e91ff4c9adad42ad
Sathabhron <thahi5_vi@hotmail.com>
2026-02-23 23:37:11 +0700

    feat: initialize new Rust project with basic setup, example code, and updated gitignore.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rust Just Learn
+link : https://google.github.io/comprehensive-rust/running-the-course/course-structure.html
+
+## 1. Introduction
+
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &s; // immutable borrow
+    let r2 = &s; // immutable borrow
+    println!("{} and {}", r1, r2);
+
+    let r3 = &mut s; // mutable borrow
+    r3.push_str(" world");
+    println!("{}", r3);
+
+    let r4 = r3.clone(); // clone s 
+    println!("{}", r4);
+    let len = calculate_length(&r4);
+    println!("The length of '{}' is {}.", r4, len);
+
+    let mut r5 = r4.clone(); // clone s
+    change(&mut r5);
+    println!("{}", r5);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(s: &mut String) {
+    s.push_str(" world 3");
+}
