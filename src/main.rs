3a1c2a652d26850059e08947f5b4cc70fac76ef3
Sathabhron <thahi5_vi@hotmail.com>
2026-02-23 23:51:58 +0700

    feat: Add `User` struct with constructor and greeting method, and demonstrate its usage.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+struct User {
+    id: u32,
+    name: String,
+    active: bool,
+}
+
+impl User { // Struct method
+    fn new(id: u32, name: String) -> Self { // Associated function (constructor style)
+        Self {
+            id,
+            name,
+            active: true,
+        }
+    }
+    fn greet(&self) { // immutable borrow
+        println!("Hello {}", self.name);
+    }
+}
+
 fn main() {
     let mut s = String::from("hello");
 
@@ -17,6 +36,18 @@ fn main() {
     let mut r5 = r4.clone(); // clone s
     change(&mut r5);
     println!("{}", r5);
+
+    let user = User {
+        id: 1,
+        name: String::from("Tha"),
+        active: true,
+    };
+    println!("{} is {} years old and active is {}", user.name, user.id, user.active);
+    user.greet();
+
+    let user2 = User::new(2, String::from("Karina"));
+    println!("{} is {} years old and active is {}", user2.name, user2.id, user2.active);
+    user2.greet();
 }
 
 fn calculate_length(s: &String) -> usize { // immutable borrow
